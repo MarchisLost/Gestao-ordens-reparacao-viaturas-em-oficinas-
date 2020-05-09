777759f0af1dafb6ecbfec578834a996be9e0011
David Pinto <MarchisLost@users.noreply.github.com>
2020-05-09 16:09:48 +0100

    added elephant to postgreSQL

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require('sequelize');
 
 // Option 1: Passing a connection URI
-module.exports = new Sequelize('postgres://postgres:PedroDavid123@localhost:5432/AutoShopManager') // Example for postgres
+//module.exports = new Sequelize('postgres://postgres:PedroDavid123@localhost:5432/AutoShopManager') // Example for postgres
+module.exports = new Sequelize('postgres://yzeyqail:9vy3_8okrryjz87bskwiEC_50FQkTSv2@kandula.db.elephantsql.com:5432/yzeyqail')
 
 // Option 2: Passing parameters separately (other dialects)
 const sequelize = new Sequelize('database', 'username', 'password', {
