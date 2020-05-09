5f7d1f38dbd962b8a79338152428c4f733ac1046
Miguel Graca <pmcg4800@protonmail.com>
2020-05-09 15:18:05 +0100

    Adicionada Vista do Responsavel e Admin

--- src/app.js
+++ src/app.js
@@ -18,6 +18,14 @@ app.get('/workview', (req, res) => {
 app.get('/rececionista', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', '/html/rececionista.html'))
 })
+
+app.get('/responsavel', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', '/html/responsavel.html'))
+})
+
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', '/html/admin.html'))
+})
  
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`)
