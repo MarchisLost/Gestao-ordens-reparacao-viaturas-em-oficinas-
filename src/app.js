06c9383511838f04e72b1015b0e88f5681bbc9a7
David Pinto <davidpinto15@gmail.com>
2020-05-12 11:27:23 +0100

    changed route name to roleRouter

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const db = require('./db/db')
 const {getLogin} = require('./db/user')
-const loginRouter = require('./routers/routes')
+const roleRouter = require('./routers/routes')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -13,36 +13,7 @@ app.use(express.static(publicDirectoryPath))
 app.use(bodyParser.urlencoded({extended: false}))
 
 //Routes  
-app.use(loginRouter)
-
-/* app.get('/mecanicLogin', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', '/html/mecLogin.html'))
-})
-
-app.get('/workview', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', '/html/workview.html'))
-})
-
-app.get('/rececionista', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', '/html/rececionista.html'))
-})
-
-app.get('/responsavel', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', '/html/responsavel.html'))
-})
-
-app.get('/admin', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', '/html/admin.html'))
-})
-
-app.get('/cliente', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', '/html/cliente.html'))
-})
-
-app.get('/vistaGeral', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', '/html/vistaGeral.html'))
-}) */
-
+app.use(roleRouter)
 
 //Login attempt 
 app.post('/loginAtempt', async (req, res) => {
