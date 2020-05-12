a6f6236ba165679f798e34f671d208fc78169463
David Pinto <davidpinto15@gmail.com>
2020-05-12 15:55:26 +0100

    added .env file ye ik u can check the pass on previous commits, who cares?

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,12 @@
 //const { Sequelize } = require('sequelize');
 const Pool = require('pg').Pool
+require('dotenv').config();
 
 //passing the necessary information for the postgresql bd
 module.exports =  new Pool({
-  user: 'yzeyqail',
-  host: 'kandula.db.elephantsql.com',
-  database: 'yzeyqail',
-  password: '9vy3_8okrryjz87bskwiEC_50FQkTSv2',
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: '5432'
 })
