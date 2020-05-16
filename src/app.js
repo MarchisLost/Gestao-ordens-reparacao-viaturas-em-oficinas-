3250d9fe8dd865ec2fd66729cfa225cd767d97d5
David Pinto <davidpinto15@gmail.com>
2020-05-16 19:20:30 +0100

    added functionality to all the logouts

--- src/app.js
+++ src/app.js
@@ -1,29 +1,41 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
+const hbs = require('hbs')
 
 //File requires
 require('dotenv').config();
 const db = require('./db/db')
-const roleRouter = require('./routers/roleRoutes')
-const loginRouter = require('./routers/loginRoute')
+const roleRoute = require('./routers/roleRoute')
+const loginRoute = require('./routers/loginRoute')
+const fileRoute = require('./routers/fileRoute')
+const {getLogin, getVeiculo} = require('./db/templates')
 
 const saltRounds = 10 //number of salt rounds, best one in terms of security&speed
 
 const app = express()
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '../public/html'))
 
 app.use(express.static(publicDirectoryPath))
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false})) 
 
 //TODO validation on the login form
 
 //Routes  
-app.use(roleRouter)
+app.use(roleRoute)
+app.use(fileRoute)
 //TODO find a way to display that the username or password are wrong!!
-app.use(loginRouter)
+app.use(loginRoute)
 
+//test for the function on db/templates to get the vehicle
+app.get('/veiculo/:id', async (req, res) => {
+  const data = await getVeiculo(req.params.id)
+  console.log("data", data)
+  res.json({"data": data})
+})
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`)
 })
