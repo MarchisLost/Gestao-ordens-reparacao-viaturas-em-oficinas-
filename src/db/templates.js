c4e4f5ac927dd2ca9e151bb040e33ed14c2503cb
David Pinto <davidpinto15@gmail.com>
2020-05-16 19:21:43 +0100

    added function to get the vehicle info

--- src/db/templates.js
+++ src/db/templates.js
@@ -9,6 +9,16 @@ const getLogin = async (username) => {
   return data.rows[0] ? data.rows[0] : null 
 }
 
+//Get vehicle info
+const getVeiculo = async (matricula) => {
+  let data = await db.query(
+    'SELECT * FROM veiculo WHERE matricula=$1', [matricula])
+    .catch(e => console.error(e.stack))
+
+    return data.rows[0] ? data.rows[0] : null
+}
+
 module.exports = {
-  getLogin
+  getLogin,
+  getVeiculo
 }
