b1075b60c03926096b9d80c2efb6ea44236c6053
David Pinto <MarchisLost@users.noreply.github.com>
2020-06-14 12:31:41 +0100

    deleted useless comments

--- src/app.js
+++ src/app.js
@@ -8,16 +8,12 @@ const db = require('./db/db')
 const roleRoute = require('./routers/roleRoute')
 const loginRoute = require('./routers/loginRoute')
 const fileRoute = require('./routers/fileRoute')
-// const {getVeiculo, getVeiculoFuncionario, getClient, getOrcamento} = require('./db/templates')
-
-// const saltRounds = 10 //number of salt rounds, best one in terms of security&speed
 
 const app = express()
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.set('view engine', 'hbs');
-//app.set('views', path.join(__dirname, '../public/html'))
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true })); 
